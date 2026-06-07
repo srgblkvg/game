e65eff76e0d1c77271b05dc1e214ebf8ce60f4ce
srgblkvg <srgblkvg@users.noreply.github.com>
2026-06-07 20:06:40 +1000

    Откат ширины колонок

--- client/src/components/RightSidebar.tsx
+++ client/src/components/RightSidebar.tsx
@@ -12,7 +12,7 @@ export default function RightSidebar() {
     };
 
     return (
-        <div className="flex flex-col gap-4 min-w-0 overflow-hidden w-full sm:w-[200px] sm:flex-shrink-0">
+        <div className="flex flex-col gap-4 min-w-0 overflow-hidden w-full sm:w-auto">
             <QuestsBlock onHighlight={handleHighlight} />
             <TournamentBanner />
             <RatingBlock />
