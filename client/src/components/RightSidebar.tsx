c604737349916a174e170b68c1046a7f38fb461e
srgblkvg <srgblkvg@users.noreply.github.com>
2026-06-13 01:05:23 +1000

    Заголовок Турниров обратно на место (absolute top-3), overflow на inner div

--- client/src/components/RightSidebar.tsx
+++ client/src/components/RightSidebar.tsx
@@ -40,9 +40,9 @@ export default function RightSidebar() {
 
             {/* Панель */}
             <div
-                className={`fixed right-0 top-[80px] z-20 w-[340px] h-[calc(100vh-80px-40px)] bg-[var(--color-bg-primary)]/60 backdrop-blur-xl border-l border-[var(--color-border-default)] overflow-y-auto p-3 pt-8 pb-10 shadow-2xl transition-transform duration-200 ease-out ${open ? 'translate-x-0' : 'translate-x-full'}`}
+                className={`fixed right-0 top-[80px] z-20 w-[340px] h-[calc(100vh-80px-40px)] bg-[var(--color-bg-primary)]/60 backdrop-blur-xl border-l border-[var(--color-border-default)] shadow-2xl transition-transform duration-200 ease-out ${open ? 'translate-x-0' : 'translate-x-full'}`}
             >
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4 overflow-y-auto h-full p-3 pt-8 pb-10">
                     <QuestsBlock onHighlight={(type) => { handleHighlight(type); if (type) setOpen(false); }} />
                     <TournamentBanner />
                     <RatingBlock />
