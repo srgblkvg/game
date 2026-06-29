177225268ce341658ce9737dc58d28bf75ea5e16
srgblkvg <srgblkvg@users.noreply.github.com>
2026-06-29 20:28:53 +1000

    чат: переименовал колонки в lowercase senderguild/senderguildid

--- server/src/db/schema.sql
+++ server/src/db/schema.sql
@@ -384,8 +384,8 @@ CREATE TABLE IF NOT EXISTS chat_messages (
   targetId INTEGER,
   content TEXT NOT NULL,
   item_data TEXT,
-  "senderGuild" TEXT,
-  "senderGuildId" INTEGER,
+  senderguild TEXT,
+  senderguildid INTEGER,
   createdAt TIMESTAMPTZ DEFAULT NOW()
 );
 
