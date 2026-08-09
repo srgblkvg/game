1e205eb39c914c1c00ce3418d86b2ae9a0829312
srgblkvg <srgblkvg@users.noreply.github.com>
2026-08-09 15:19:03 +1000

    данж: рейтинги в две колонки на десктопе

--- client/src/pages/DungeonPage.tsx
+++ client/src/pages/DungeonPage.tsx
@@ -385,8 +385,9 @@ export default function DungeonPage() {
 
             {(leaderboard.topFloor.length > 0 || leaderboard.topReward.length > 0) && (
                 <div className="border-t border-[var(--color-border-light)] pt-3 mt-3">
+                    <div className="flex flex-col md:flex-row gap-3">
                     {leaderboard.topFloor.length > 0 && (
-                        <div className="mb-3">
+                        <div className="md:w-1/2">
                             <h4 className="text-xs font-bold mb-1">🏆 Рейтинг этажа</h4>
                             {leaderboard.topFloor.map((r: any, i: number) => (
                                 <div key={i} className="flex justify-between text-xs py-0.5">
@@ -397,7 +398,7 @@ export default function DungeonPage() {
                         </div>
                     )}
                     {leaderboard.topReward.length > 0 && (
-                        <div>
+                        <div className="md:w-1/2">
                             <h4 className="text-xs font-bold mb-1">💰 Рейтинг награды</h4>
                             {leaderboard.topReward.map((r: any, i: number) => (
                                 <div key={i} className="flex justify-between text-xs py-0.5">
@@ -407,6 +408,7 @@ export default function DungeonPage() {
                             ))}
                         </div>
                     )}
+                    </div>
                 </div>
             )}
         </Card>
